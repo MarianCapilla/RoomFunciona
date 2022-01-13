b556d21800f9d5ef7ad07a477ca782951c332db9
MARIAN\dell <mch.formacion@gmail.com>
2022-01-13 20:24:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/roomfunciona/BorrarDatos.kt
@@ -0,0 +1,11 @@
+package com.example.roomfunciona
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BorrarDatos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_borrar_datos)
+    }
+}
---
+++ app/src/main/java/com/example/roomfunciona/ModificarDatos.kt
@@ -0,0 +1,11 @@
+package com.example.roomfunciona
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModificarDatos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modificar_datos)
+    }
+}
